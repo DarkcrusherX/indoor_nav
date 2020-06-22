f7ee84d927ee2a8204b326883863cc3ecc9e8735
Ahal <45266108+DarkcrusherX@users.noreply.github.com>
2020-06-22 18:47:17 +0530

    Add files via upload

---
+++ src/localposition.py
@@ -0,0 +1,30 @@
+import rospy
+from gazebo_msgs.msg import ModelStates
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseStamped
+
+
+rospy.init_node('realsense', anonymous=True)
+
+def pos_callback(pos_msg):
+  position = Pose()
+  position = pos_msg.pose[2]
+  #print(position)
+  pos = PoseStamped()
+  pos.header.stamp = rospy.Time.now()
+  pos.header.frame_id = "map"
+  pos.pose.position.x = round(position.position.x,12)
+  pos.pose.position.y = round(position.position.y,12)
+  pos.pose.position.z = round(position.position.z,12)
+  pos.pose.orientation.x = round(position.orientation.x,12)
+  pos.pose.orientation.y = round(position.orientation.y,12)
+  pos.pose.orientation.z = round(position.orientation.z,12)
+  pos.pose.orientation.w = round(position.orientation.w,12)
+  print(pos)
+  pub_pos.publish(pos)
+
+
+sub_pos = rospy.Subscriber("/gazebo/model_states", ModelStates, pos_callback)
+pub_pos = rospy.Publisher('/mavros/vision_pose/pose', PoseStamped, queue_size=30)
+
+rospy.spin()
